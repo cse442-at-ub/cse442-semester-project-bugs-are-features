a32b4436dfbe06d04085905ad50149fcda4acd73
Lisander Lopez <lisander@buffalo.edu>
2020-02-15 14:37:08 -0500

    Added 4 basic buttons, functionality will come later

--- lib/ghost_main.dart
+++ lib/ghost_main.dart
@@ -1,19 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:ghost_app/ghost_button.dart';
 
-class GhostMain extends StatelessWidget {
+class GhostMain extends StatefulWidget {
+  /// The app wide preferences.
+  final SharedPreferences _prefs;
+
+  /// Called as a function when a ghost is released.
+  final VoidCallback _ghostReleased;
+  GhostMain(this._prefs, this._ghostReleased);
+
+  @override
+  _GhostMainState createState() => _GhostMainState(_prefs, _ghostReleased);
+}
+
+class _GhostMainState extends State<GhostMain> {
+  _GhostMainState(this._prefs, this._ghostReleased);
+
   /// The app wide preferences.
   final SharedPreferences _prefs;
 
   /// Called as a function when a ghost is released.
   final VoidCallback _ghostReleased;
 
-  GhostMain(this._prefs, this._ghostReleased);
+  var options = ["Hello", "What is your name?", "Blah", "Foo"];
 
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Container(
+        margin: EdgeInsets.only(top: 180),
+        child: Column(
+          children: <Widget>[
+            Button(options[0]),
+            Button(options[1]),
+            Button(options[2]),
+            Button(options[3]),
+          ],
+        ),
+        /*        
         child: Text(
           "GhostMain",
           textAlign: TextAlign.center,
@@ -21,8 +46,14 @@ class GhostMain extends StatelessWidget {
             color: Colors.blueGrey,
             fontSize: 60.0,
           ),
-        ),
+          */
       ),
     );
   }
+  /*
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+  */
 }
