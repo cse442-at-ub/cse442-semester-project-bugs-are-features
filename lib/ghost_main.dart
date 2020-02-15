7c23b418032fd89e72957ae1456ed9a317fb8211
Anarghya <anarghya@buffalo.edu>
2020-02-15 17:09:14 -0500

    added ghost asset to the project

--- lib/ghost_main.dart
+++ lib/ghost_main.dart
@@ -24,7 +24,7 @@ class _GhostMainState extends State<GhostMain> {
 
   var options = ["Hello", "What is your name?", "Blah", "Foo"];
   var response = ["Hi", "Ghost", "Bloo", "Bar"];
-  var currentResponse = "Temporary";
+  var currentResponse = "Ghost comes here";
 
   void buttonHandler(int id) {
     setState(() {
@@ -34,43 +34,53 @@ class _GhostMainState extends State<GhostMain> {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        margin: EdgeInsets.only(top: 100),
-        child: Column(
-          children: <Widget>[
-            Text(
-              currentResponse,
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                color: Colors.brown,
-                fontSize: 42,
+    return Container(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Image(
+            alignment: Alignment.topCenter,
+            image: AssetImage("assets/ghost1.png"),
+          ),
+          Text(
+            currentResponse,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.brown,
+              fontSize: 42,
+            ),
+          ),
+          ButtonBar(
+            alignment: MainAxisAlignment.center,
+            children: <Widget>[
+              RaisedButton(
+                child: Text(options[0]),
+                color: Colors.red,
+                textColor: Colors.yellow,
+                padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                splashColor: Colors.grey,
+                onPressed: () => buttonHandler(0),
               ),
-            ),
-            GridView.count(
-              shrinkWrap: true,
-              crossAxisCount: 2,
-              children: <Widget>[
-                RaisedButton(
-                  onPressed: () => buttonHandler(0),
-                  child: Text(options[0]),
-                ),
-                RaisedButton(
-                  onPressed: () => buttonHandler(1),
-                  child: Text(options[1]),
-                ),
-                RaisedButton(
-                  onPressed: () => buttonHandler(2),
-                  child: Text(options[2]),
-                ),
-                RaisedButton(
-                  onPressed: () => buttonHandler(3),
-                  child: Text(options[3]),
-                ),
-              ],
-            ),
-          ],
-        ),
+              RaisedButton(
+                onPressed: () => buttonHandler(1),
+                child: Text(options[1]),
+              ),
+            ],
+          ),
+          ButtonBar(
+            alignment: MainAxisAlignment.center,
+            children: <Widget>[
+              RaisedButton(
+                onPressed: () => buttonHandler(2),
+                child: Text(options[2]),
+              ),
+              RaisedButton(
+                onPressed: () => buttonHandler(3),
+                child: Text(options[3]),
+              ),
+            ],
+          )
+        ],
       ),
     );
   }
