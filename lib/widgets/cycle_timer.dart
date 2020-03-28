d80b15dd604b6c1c980efc36c4730db1fe273538
chinmayeebandal <cbandal@buffalo.edu>
2020-03-28 17:03:24 -0400

    #106 Initial commit

---
+++ lib/widgets/cycle_timer.dart
@@ -0,0 +1,14 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:ghost_app/models/ghost.dart';
+
+class CycleTimer extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return null;
+  }
+  
+}
