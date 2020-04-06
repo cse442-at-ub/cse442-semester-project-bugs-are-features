6a97b039bd20b8d96fda06336843faf4a199b73e
chinmayeebandal <cbandal@buffalo.edu>
2020-04-05 22:28:14 -0400

    #108 The energy now increases by 75% on lighting candle. It was set to 100 earlier.

--- lib/models/energy.dart
+++ lib/models/energy.dart
@@ -4,8 +4,8 @@ double energyInit = 100;
 
 ///Sets the energy to 100 when the candle is lit
 void setEnergyCandleLit(bool isLit){
-  if(isLit){
-      energyInit = 100;
+  if(isLit && ((energyInit + 0.75) <= 100)){
+      energyInit += 0.75;
       debugPrint("Candle lit: Energy set to $energyInit");
   }
 }
