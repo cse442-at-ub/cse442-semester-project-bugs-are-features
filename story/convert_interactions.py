d7be559f7a7641b7cda974fe450fdbc30c02f593
mferrera <matt.ferrera@gmail.com>
2020-04-30 15:38:28 -0400

    Remove dead code from interactions conversion script (#146)

--- story/convert_interactions.py
+++ story/convert_interactions.py
@@ -3,38 +3,6 @@ import csv
 import os
 import sys
 
-# Sets of SQL statements we're generating
-interactions = set()
-
-def ghost_node(pid):
-    # The current ghost node we're working on
-    node = data[pid - 1]
-
-    # pids start from 1, not 0
-    text = node["text"].split('\n')[0]
-
-    # Get list of user responses this ghost resp links to
-    pids = ""
-    # The response id's we'll use later
-    rids = []
-
-    if 'links' in node:
-        for resp in node["links"]:
-            pids = pids + resp["pid"] + ","
-            rids.append(int(resp["pid"]))
-    # Cut trailing ','
-    pids = pids[:-1]
-
-
-    if statement in ghost_resps:
-        return
-    else:
-        ghost_resps.add(statement)
-
-    # Now add the user responses
-    for rid in rids:
-        user_node(rid)
-
 def main(argv):
     if len(argv) != 2:
         print("Usage: {} <ghost id>".format(argv[0]))
