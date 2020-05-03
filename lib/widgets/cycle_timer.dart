45ad4f26c4df4d54cf80bb1e4c0a8ffd650b975d
Nicolas Barrios <nbarrios1337@gmail.com>
2020-05-03 19:38:15 -0400

    Removed unused imports

--- lib/widgets/cycle_timer.dart
+++ lib/widgets/cycle_timer.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:ghost_app/models/energy.dart';
 import 'package:ghost_app/models/game.dart';
 import 'package:ghost_app/settings.dart' as Settings;
 
